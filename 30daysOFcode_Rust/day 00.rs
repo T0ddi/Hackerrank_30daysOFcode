3f98867a0f687ddb057a62b5f39f47be688cc850
toddi <github@agilemaster.dev>
2024-09-03 11:08:07 +0200

    Delete 30daysOFcode_Rust directory

--- 30daysOFcode_Rust/day 00.rs
+++
@@ -1,8 +0,0 @@
-use std::io;
-fn main() {
-    let mut line = String::new();
-    io::stdin()
-        .read_line(&mut line)
-        .expect("Failed to read line.");
-    println!("Hello, World.\n{}", line);
-}
